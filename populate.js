98a611fb33946616ac46c3fa70915e12ea577f68
Terrence <yl124@rice.edu>
2018-07-01 00:35:34 -0500

    update populate code; buggyyy:

--- populate.js
+++ populate.js
@@ -1,5 +1,6 @@
 var db = require('./db');
 var Request = require('./models/request');
+var Case = require('./models/case');
 const fs = require('fs');
 
 
@@ -9,16 +10,67 @@ var data = JSON.parse(fs.readFileSync('./data.json', 'utf-8', function(err, res)
 
 var result = data.result;
 
+
+
+/**
+ * Add to DB
+ */
 result.forEach(function(item, index) {
-    Request.create({
-        latitude: item.latitude,
-        longitude: item.longitude,
-        device_id: 0
+    // Check Case
+    Case.find({case_id: item.case_id}).exec((err, res) => {
+        if (res.length == 0) {
+            console.log("Zero for ", item.case_id, res);
+            // // Create case
+            // var c = new Case();
+            // c.case_id = item.case_id;
+            // c.resolved = false;
+            // c.save(function(err, c) {
+                
+            //     var request = new Request();
+            //     request.case_id = c._id;
+            //     request.devide_id = 0;
+            //     request.longitude = item.longitude;
+            //     request.latitude = item.latitude;
+                
+            //     request.save(function(err, req) {
+            //         if (err) {
+            //             console.log(err);
+            //             return err;
+            //         }
+            //     });
+            // });
+        } else {
+            console.log(res[0]);
+            // var request = new Request();
+            // request.case_id = res[0]._id;
+            // request.devide_id = 0;
+            // request.longitude = item.longitude;
+            // request.latitude = item.latitude;
+            
+            // request.save(function(err, req) {
+            //     if (err) {
+            //         console.log(err);
+            //         return err;
+            //     }
+
+            //     console.log(req);
+            // });
+        }
     });
-})
+});
 
-console.log("Finish create request")
+// result.forEach(element => {
+//     Case.find({case_id: element.case_id}).exec((err, res) => {
+//         if (res.length > 1) {
+//             for (var i = 1; i < res.length; i++) {
+//                 Case.deleteOne({ _id: res[i]._id}, function (err) {
+//                     console.log(err);
+//                     Case.find({_id: res[i]._id }).exec((err, r) => {
+//                         console.log(r);
+//                     })
+//                 });
+//             }
+//         }
+//     });
+// });
 
-Request.find({}).exec(function(err, res) {
-    console.log(res);
-})
